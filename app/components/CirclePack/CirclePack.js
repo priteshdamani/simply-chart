e937fa4362c2f206e00d7cc73a70394e58158469
Wilkins <wilkinsf@gmail.com>
2016-01-06 20:23:54 -0500

    Setting up for zoom function

--- app/components/CirclePack/CirclePack.js
+++ app/components/CirclePack/CirclePack.js
@@ -11,54 +11,64 @@ const CirclePack = React.createClass({
     r: React.PropTypes.number.isRequired
   },
 
-  _renderBubbleZoom() {
-    var w = this.props.width,
+  componentDidMount(){
+    this._renderCircle();
+    //this.setState({
+    //  data: {}
+    //});
+  },
+
+  _zoom(){
+
+  },
+
+  _renderCircle() {
+    const w = this.props.width,
         h = this.props.height,
-        r = this.props.r,
-        x = d3.scale.linear().range([0, r]),
+        r = this.props.r;
+    var x = d3.scale.linear().range([0, r]),
         y = d3.scale.linear().range([0, r]),
         node,
         root;
-
     var pack = d3.layout.pack()
         .size([r, r])
         .value((d)=> {
           return d.size;
         });
 
-    var vis = d3.select("body").insert("svg:svg", "h2")
-        .attr("width", w)
-        .attr("height", h)
-        .append("svg:g")
-        .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
+    var vis = d3.select('body').insert('svg:svg', 'h2')
+        .attr('width', w)
+        .attr('height', h)
+        .append('svg:g')
+        .attr('transform', 'translate(' + (w - r) / 2 + ',' + (h - r) / 2 + ')');
 
     function zoom(d, i) {
-      var k = r / d.r / 2;
+      const k = r / d.r / 2;
       x.domain([d.x - d.r, d.x + d.r]);
       y.domain([d.y - d.r, d.y + d.r]);
 
       var t = vis.transition()
           .duration(d3.event.altKey ? 7500 : 750);
 
-      t.selectAll("circle")
-          .attr("cx", (d)=> {
+      t.selectAll('circle')
+          .attr('cx', (d)=> {
             return x(d.x);
           })
-          .attr("cy", (d)=> {
+          .attr('cy', (d)=> {
             return y(d.y);
           })
-          .attr("r", (d)=> {
+          .attr('r', (d)=> {
             return k * d.r;
           });
 
-      t.selectAll("text")
-          .attr("x", (d)=> {
+      t.selectAll('text')
+          .attr('x', (d)=> {
             return x(d.x);
           })
-          .attr("y", (d)=> {
+          .attr('y', (d)=> {
             return y(d.y);
           })
-          .style("opacity", (d)=> {
+          .style('opacity', (d)=> {
             return k * d.r > 20 ? 1 : 0;
           });
 
@@ -67,63 +77,57 @@ const CirclePack = React.createClass({
     }
 
 
-    d3.json("./flare.json", (data)=> {
+    d3.json('./flare.json', (data)=> {
       node = root = data;
 
       const nodes = pack.nodes(root);
 
-      vis.selectAll("circle")
+      vis.selectAll('circle')
           .data(nodes)
-          .enter().append("svg:circle")
-          .attr("class", (d)=> {
-            return d.children ? "parent" : "child";
+          .enter().append('svg:circle')
+          .attr('class', (d)=> {
+            return d.children ? 'parent' : 'child';
           })
-          .attr("cx", (d)=> {
+          .attr('cx', (d)=> {
             return d.x;
           })
-          .attr("cy", (d)=> {
+          .attr('cy', (d)=> {
             return d.y;
           })
-          .attr("r", (d)=> {
+          .attr('r', (d)=> {
             return d.r;
           })
-          .on("click", (d)=> {
+          .on('click', (d)=> {
             return zoom(node == d ? root : d);
           });
 
-      vis.selectAll("text")
+      vis.selectAll('text')
           .data(nodes)
-          .enter().append("svg:text")
-          .attr("class", (d)=> {
-            return d.children ? "parent" : "child";
+          .enter().append('svg:text')
+          .attr('class', (d)=> {
+            return d.children ? 'parent' : 'child';
           })
-          .attr("x", (d)=> {
+          .attr('x', (d)=> {
             return d.x;
           })
-          .attr("y", (d)=> {
+          .attr('y', (d)=> {
             return d.y;
           })
-          .attr("dy", ".35em")
-          .attr("text-anchor", "middle")
-          .style("opacity", (d)=> {
+          .attr('dy', '.35em')
+          .attr('text-anchor', 'middle')
+          .style('opacity', (d)=> {
             return d.r > 20 ? 1 : 0;
           })
           .text((d)=> {
             return d.name;
           });
 
-      d3.select(window).on("click", ()=> {
+      d3.select(window).on('click', ()=> {
         zoom(root);
       });
     });
   },
 
-  componentDidMount(){
-    this._renderBubbleZoom();
-    //this.setState({
-    //  data: {}
-    //});
-  },
   render(){
     return (
         <div>
