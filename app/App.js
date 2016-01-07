e7a2931aa280837818986369b7b1dda2e276e0d8
Wilkins <wilkinsf@gmail.com>
2016-01-06 19:17:12 -0500

    Removing irrelevant code

--- app/App.js
+++ app/App.js
@@ -6,14 +6,6 @@ import ReactDOM from 'react-dom';
 
 // require `react-d3-core` for Chart component, which help us build a blank svg and chart title.
 const Chart = require('react-d3-core').Chart;
-// require `react-d3-basic` for Line chart component.
-const LineChart = require('react-d3-basic').LineChart;
-
-
-// TODO: Implement socket.io
-// https://github.com/socketio/socket.io-client
-// const socketServer = require('socket.io-client')('http://localhost');
-// const socketClient = require('socket.io-client')('http://localhost');
 
 const App = React.createClass({
   getInitialState () {
@@ -132,42 +124,6 @@ const App = React.createClass({
     });
   },
 
-
-  _renderChart(){
-    const width = 700,
-        height = 300,
-        margins = {left: 100, right: 100, top: 50, bottom: 50},
-        title = 'User sample',
-    // chart series,
-    // field: is what field your data want to be selected
-    // name: the name of the field that display in legend
-    // color: what color is the line
-        chartSeries = [
-          {
-            field: 'BMI',
-            name: 'BMI',
-            color: '#ff7f0e'
-          }
-        ],
-    // your x accessor
-        x = (d)=> {
-          return d.index;
-        };
-    return <Chart title={title}
-                  width={width}
-                  height={height}
-                  margins={margins}>
-      <LineChart margins={margins}
-                 title={title}
-                 data={this.state.data}
-                 width={width}
-                 height={height}
-                 chartSeries={chartSeries}
-                 x={x}/>
-    </Chart>
-  },
-
-
   render(){
     return (
         <div>
@@ -175,18 +131,9 @@ const App = React.createClass({
             <input type="text" placeholder="websocket url"/>
           </header>
           <section>
-            {this._renderChart()}
           </section>
         </div>
     )
   }
 });
 ReactDOM.render(<App/>, document.getElementById('app'));
-
-/* Example of getting a query string
- * import queryString from 'query-string';
- * const parsed = queryString.parse(location.search);
- * goto: http://localhost:8080/?url=test-websocket-url
- * test: console.log(parsed);
- * src: https://github.com/sindresorhus/query-string
- * */
